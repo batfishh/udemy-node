dbec768c43d07d35a4641ae0e7c7fc176e98ad7a
Ashwin Kumar Singh <ashwinkumarsingh@Ashwins-MacBook-Pro.local>
2020-11-22 20:18:05 +0530

    commiting after express intro

---
+++ web-server/src/app.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+
+app.use(express.static(path.join(__dirname,'../public')))
+
+
+
+// app.get('/help',(req,res)=>{
+//     res.send("help")
+// })
+
+// app.get('/about',(req,res)=>{
+//     res.send('About')
+// })
+
+app.get('/weather',(req,res)=>{
+    res.send('Weather')
+})
+
+
+
+app.listen(3000,()=>{
+    console.log("server started on port 3000")
+})
