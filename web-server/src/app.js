57d2d6bf46a5ab2ec2e72f6390b05e6447b36158
batfishh <ashwin27live@gmail.com>
2020-11-23 23:07:01 +0530

    committin after making 404 page, PC

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,20 +1,26 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const hbs = require('hbs')
+const viewsPath = path.join(__dirname,'../templates/views')
+const partialsPath = path.join(__dirname,'../templates/partials')
+
 app.set('view engine','hbs')
 app.use(express.static(path.join(__dirname,'../public')))
+app.set('views',viewsPath)
+hbs.registerPartials(partialsPath)
 
 app.get('',(req,res)=>{
-    res.render('index',{abc:"TESTSTUFF"})
+    res.render('index',{title:"Root page",name:"batfish"})
 })
 
 app.get('/about',(req,res)=>{
-    res.render('about',{test:"About page@!"})
+    res.render('about',{title:"About page@!",name:"batfish"})
 })
 
 
 app.get('/help',(req,res)=>{
-    res.render('help',{title:"Help page@!"})
+    res.render('help',{title:"Help page@!",name:"batfish"})
 })
 // app.get('/help',(req,res)=>{
 //     res.send("help")
@@ -28,6 +34,13 @@ app.get('/weather',(req,res)=>{
     res.send('Weather')
 })
 
+app.get('/help/*',(req,res)=>{
+    res.render('notfound',{title:"404 Page",err:"couldn't find help file!",name:"batfish"})
+})
+app.get('*',(req,res)=>{
+    res.render('notfound',{title:"404 Page",err:"couldn't find ,404!",name:"batfish"})
+    
+})
 
 
 app.listen(3000,()=>{
